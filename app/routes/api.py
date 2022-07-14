446eacfbdbc0727820faf2a57c799cd0e8e03407
Spencer Hulse <hulse.spencer@gmail.com>
2022-07-13 22:18:04 -0400

    Added wrapper to authenticate users and protect routes.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
@@ -61,6 +62,7 @@ def login():
 
 
 @bp.route("/comments", methods=["POST"])
+@login_required
 def comment():
     data = request.get_json()
     db = get_db()
@@ -83,6 +85,7 @@ def comment():
 
 
 @bp.route("/posts", methods=["POST"])
+@login_required
 def create():
     data = request.get_json()
     db = get_db()
@@ -105,6 +108,7 @@ def create():
 
 
 @bp.route("/posts/<id>", methods=["PUT"])
+@login_required
 def update(id):
     data = request.get_json()
     db = get_db()
@@ -122,6 +126,7 @@ def update(id):
 
 
 @bp.route("/posts/<id>", methods=["DELETE"])
+@login_required
 def delete(id):
     db = get_db()
 
@@ -137,6 +142,7 @@ def delete(id):
 
 
 @bp.route("/posts/upvote", methods=["PUT"])
+@login_required
 def upvote():
     data = request.get_json()
     db = get_db()
