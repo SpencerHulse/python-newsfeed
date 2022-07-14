d536fae388367a170af0029d3db2935031c8d2ec
Spencer Hulse <hulse.spencer@gmail.com>
2022-07-13 21:25:34 -0400

    Added routes to comment and upvote.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 
 bp = Blueprint("api", __name__, url_prefix="/api")
@@ -58,3 +58,43 @@ def login():
     session["loggedIn"] = True
 
     return jsonify(id=user.id)
+
+
+@bp.route("/comments", methods=["POST"])
+def comment():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # Create a new comment
+        newComment = Comment(
+            comment_text=data["comment_text"],
+            post_id=data["post_id"],
+            user_id=session.get("user_id"),
+        )
+
+        db.add(newComment)
+        db.commit()
+    except:
+        db.rollback()
+        return jsonify(message="Comment failed"), 500
+
+    return jsonify(id=newComment.id)
+
+
+@bp.route("/posts/upvote", methods=["PUT"])
+def upvote():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # Create a new vote with the incoming id and session id
+        newVote = Vote(post_id=data["post_id"], user_id=session.get("user_id"))
+
+        db.add(newVote)
+        db.commit()
+    except:
+        db.rollback()
+        return jsonify(message="Upvote failed"), 500
+
+    return "", 204
